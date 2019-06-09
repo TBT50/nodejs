fe6dd8cd0208ba8c776cd87b36b3c9cd3a26fa1d
PiotrK <piotrek380@gmail.com>
2019-06-09 14:15:58 +0200

    Add repsonse with html

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require("http");
 
 const settings = {
-  port: 3000,
+  port: 8080,
   hostname: "127.0.0.1"
 };
 
-const server = http.createServer((request, response) => {
-  console.log("ELO");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "asdsad");
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<meta charset='utf-8'/>");
+  res.write("<title>An awesome title</title>");
+  res.write("</head>");
+  res.write("<body><h1>The best header ever!!!</h1></body>");
+  res.write("</html>");
+  res.end();
+  console.log(req.headers, req.url, req.method, res.setHeader, res.write);
 });
 
 server.listen(settings.port, settings.hostname, () => {
