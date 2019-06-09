0a1e718fe13329bb2c3cb0bd5768a20facbe0434
PiotrK <piotrek380@gmail.com>
2019-06-09 12:56:09 +0200

    Create server - just basic functionality

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const fs = require("fs");
-fs.writeFileSync("wtf.txt", "SOME AWESOME TEXT INSIDE");
+const http = require("http");
+
+const settings = {
+  port: 3000,
+  hostname: "127.0.0.1"
+};
+
+const server = http.createServer((request, response) => {
+  console.log("ELO");
+});
+
+server.listen(settings.port, settings.hostname, () => {
+  console.log(
+    `Server running at http://${settings.hostname}:${settings.port}/`
+  );
+});
