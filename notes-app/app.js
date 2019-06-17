c059e23887dc74eb4b02c9250b2e26aa54eefb58
PiotrK <piotrek380@gmail.com>
2019-06-17 16:03:56 +0200

    Add chalk, funny module to add colors to our console

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,17 @@
 const myNotes = require("./notes");
+
 const validator = require("validator");
+const chalk = require("chalk");
+
+const log = console.log;
+const newColor = log(
+  chalk.green(
+    "I am a green line " +
+      chalk.blue.underline.bold("with a blue substring") +
+      " that becomes green again!"
+  )
+);
+console.log(newColor);
 
 const notes = myNotes();
 
@@ -8,3 +20,5 @@ console.log(validator.isEmail("abc@exa$%le.com"));
 console.log(
   validator.isURL("https://wsdfdsfsdfww.wsdfdsfsdfp.ppzsdfdsfl.asdasd")
 );
+
+console.log(validator.isPostalCode("11-111", "PL"));
