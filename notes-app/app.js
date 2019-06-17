96c53d28b84402aa63d45dd06436c7895417ec73
PiotrK <piotrek380@gmail.com>
2019-06-17 22:20:18 +0200

    Add Yargs package and set commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const chalk = require("chalk");
+const yargs = require("yargs");
+
+const myNotes = require("./notes");
 
 const server = http.createServer((request, response) => {
   console.log(chalk.green("SUCCESS, request recieved!"));
@@ -10,6 +13,49 @@ const server = http.createServer((request, response) => {
   response.write("</body></html");
   response.end();
 });
+
+yargs.version("1.10.100");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note");
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing the note");
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function() {
+    console.log("Listing out the note");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Reading a note",
+  handler: function() {
+    console.log("Reading the note");
+  }
+});
+
+if (yargs.command === "add") {
+  console.log("Added a new note");
+} else if (yargs.command === "remove") {
+  console.log(chalk.bgRed("Removed a note"));
+}
+
+console.log(yargs.argv);
 
 const settings = {
   port: 8080,
