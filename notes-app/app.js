4fa2188f5766e9f369ba7ded64b4b13b7cb37d64
PiotrK <piotrek380@gmail.com>
2019-06-17 13:36:35 +0200

    Use module.exports to export function to different file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,3 @@
-// File System module has to be required
-const fs = require("fs");
+const utils = require("./utils");
 
-fs.writeFileSync(
-  "notes.txt",
-  "This is the best note ever! Dont even try to do better, cuz it's not possible"
-);
-
-const appendedText = "Node seems to be more interesting that i though123";
-
-fs.appendFileSync("notes.txt", appendedText);
-
-console.log(typeof fs);
+console.log(utils());
---
+++ notes-app/utils.js
@@ -0,0 +1,5 @@
+function name() {
+  return console.log("WTF");
+}
+
+module.exports = name;
