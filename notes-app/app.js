934f0d621357b76002c50a009ece2783bb3e4812
PiotrK <piotrek380@gmail.com>
2019-06-17 11:57:55 +0200

    append new text using .appendFile method

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,13 @@
+// File System module has to be required
 const fs = require("fs");
 
-fs.writeFileSync(
+const newFile = fs.writeFileSync(
   "notes.txt",
   "This is the best note ever! Dont even try to do better, cuz it's not possible"
 );
+
+fs.appendFile("notes.txt", "WOW", () => {
+  console.log("SAVED");
+});
+
+console.log(typeof fs);
