7011795c4add67a70e64a4348ead6531ecad14b4
PiotrK <piotrek380@gmail.com>
2019-06-17 14:41:20 +0200

    Add validation to the email address and to the URL

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,10 @@
 const myNotes = require("./notes");
+const validator = require("validator");
 
-console.log(myNotes());
+const notes = myNotes();
+
+console.log(notes);
+console.log(validator.isEmail("abc@exa$%le.com"));
+console.log(
+  validator.isURL("https://wsdfdsfsdfww.wsdfdsfsdfp.ppzsdfdsfl.asdasd")
+);
