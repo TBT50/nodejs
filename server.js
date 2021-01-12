4fc2a11e24333b07f99f48c130443a9a30319cce
PiotrK <piotrek380@gmail.com>
2021-01-12 19:55:45 +0100

    Build a simple server with a reponse

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const http = require("http");
+
+const PORT = 8080;
+const host = "localhost";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 203;
+  res.setHeader("Content-type", "text/plain");
+  res.end("bbbb");
+});
+
+server.listen(8080, host, () => {
+  console.log(`Server is running http://${host}:${PORT}/`);
+});
