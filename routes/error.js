752b5c6f7bfb5ef32cff8a6bb74282362bd9b4be
Piotrek <piotrek380@gmail.com>
2021-01-30 21:05:45 +0100

    Add 404 page

--- routes/error.js
+++ routes/error.js
@@ -5,7 +5,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "../", "views", "error.html"));
+  res.status(404).render("error");
 });
 
 module.exports = router;
