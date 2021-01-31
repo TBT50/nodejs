4dc8345b9632c71d8acc7987ebf73fa510bd22ed
Piotrek <piotrek380@gmail.com>
2021-01-31 18:28:18 +0100

    OAuth - ship the  first part (does not work yet)

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const keys = require("./config/keys");
 
 const path = require("path");
 
@@ -11,12 +15,30 @@ const indexRoutes = require("./routes/index");
 const addProductData = require("./routes/add-product");
 const errorRoutes = require("./routes/error");
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 app.use(indexRoutes);
 app.use(addProductData.routes);
